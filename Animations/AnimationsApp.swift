ad0e7008a7e56b650c485ff119df7b4c242c08d2
Ruvaid <ruvaid9@icloud.com>
2026-08-25 10:39:38 +0530

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Ruvaid on 25/08/26.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
