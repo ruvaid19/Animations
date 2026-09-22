48acafa5c7e22442db65d0aaaf5e0900c397b38d
Ruvaid <ruvaid9@icloud.com>
2026-09-22 21:31:39 +0530

    practicing animations

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -41,12 +41,12 @@ struct ContentView: View {
         ZStack {
             Rectangle()
                 .fill(.blue)
-                .frame(width: 200, height: 200)
+                .frame(width: 300, height: 300)
 
             if isShowingRed {
                 Rectangle()
                     .fill(.red)
-                    .frame(width: 200, height: 200)
+                    .frame(width: 300, height: 300)
                     .transition(.pivot)
             }
         }
